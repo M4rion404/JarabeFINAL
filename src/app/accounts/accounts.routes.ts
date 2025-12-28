2098ae0b76c4ed0ffaecda8eb787af146f7c0157
Marion <lumajimenezla@ittepic.edu.mx>
2025-12-28 01:09:03 -0700

    Primera parte del frontend creada

---
+++ src/app/accounts/accounts.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+export const ACCOUNTS_ROUTES: Routes = [
+//   {
+//     path: '',
+//     loadComponent: () =>
+//       import('./pages/account/account.component').then(
+//         m => m.AccountComponent
+//       ),
+//   },
+];
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () =>
+      import('./auth/auth.routes').then(m => m.AUTH_ROUTES),
+  },
+  {
+    path: 'accounts',
+    loadChildren: () =>
+      import('./accounts/accounts.routes').then(m => m.ACCOUNTS_ROUTES),
+  },
+  {
+    path: 'transactions',
+    loadChildren: () =>
+      import('./transactions/transactions.routes').then(
+        m => m.TRANSACTIONS_ROUTES
+      ),
+  },
+  {
+    path: '',
+    redirectTo: 'auth',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'auth',
+  },
+];
+
+export class AppRoutes {}
+
