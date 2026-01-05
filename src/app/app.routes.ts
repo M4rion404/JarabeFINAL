802fb223888842bfc34fabce1a7dbcc93b1fbda7
Marion <lumajimenezla@ittepic.edu.mx>
2026-01-04 18:39:15 -0700

    Quinta parte del frontend funcional(diseños terminados, realción-backend)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,35 +4,35 @@ import { authGuard } from './auth/guards/auth.guard';
 export const routes: Routes = [
   {
     path: 'login',
-    loadComponent: () => 
-      import('./auth/login/login').then(m => m.LoginComponent),
+    loadComponent: () => import('./auth/login/login').then((m) => m.LoginComponent),
   },
   {
     path: 'auth',
-    loadChildren: () =>
-      import('./auth/auth.routes').then(m => m.AUTH_ROUTES),
+    loadChildren: () => import('./auth/auth.routes').then((m) => m.AUTH_ROUTES),
   },
   {
     path: 'accounts',
     canActivate: [authGuard],
-    loadChildren: () =>
-      import('./accounts/accounts.routes').then(m => m.ACCOUNTS_ROUTES),
+    loadChildren: () => import('./accounts/accounts.routes').then((m) => m.ACCOUNTS_ROUTES),
   },
+  {
+    path: 'register',
+    loadComponent: () =>
+      import('./auth/register/register').then((m) => m.RegisterComponent),
+  },
+
   {
     path: 'transactions',
     loadChildren: () =>
-      import('./transactions/transactions.routes').then(
-        m => m.TRANSACTIONS_ROUTES
-      ),
+      import('./transactions/transactions.routes').then((m) => m.TRANSACTIONS_ROUTES),
   },
   {
-  path: 'dashboard',
-  loadChildren: () =>
-    import('./dashboard/dashboard.routes').then(m => m.DASHBOARD_ROUTES),
+    path: 'dashboard',
+    loadChildren: () => import('./dashboard/dashboard.routes').then((m) => m.DASHBOARD_ROUTES),
   },
   {
     path: '',
-    redirectTo: 'login',
+    redirectTo: 'register',
     pathMatch: 'full',
   },
   {
@@ -42,8 +42,7 @@ export const routes: Routes = [
   {
     path: 'transfer',
     redirectTo: 'transactions/transfer',
-  }
+  },
 ];
 
 export class AppRoutes {}
-
